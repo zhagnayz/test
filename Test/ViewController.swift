d345f3f6a091773ce7575737e4abb848d81a5fb1
Daniel Zhagany Zamora <danielzhaganyzamora@Daniels-MacBook-Pro-74.local>
2024-11-14 15:54:43 -0800

    first commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Daniel Zhagany Zamora on 11/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestTests/TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestTests.swift
+//  TestTests
+//
+//  Created by Daniel Zhagany Zamora on 11/14/24.
+//
+
+import Testing
+@testable import Test
+
+struct TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
